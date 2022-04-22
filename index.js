166e742b958d46b8320ec2aa8badf61923b635d1
Habtamu Debele <habtlig2010@gmail.com>
2022-04-22 12:40:20 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("main").remove()
+const newHeader = document.createElement('h1')
+// document.body.append(newHeader)
+newHeader.id = "victory"
+// newHeader.append('H1')
+// newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "Your name is the champion"
